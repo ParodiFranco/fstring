8b71672bce52ba635c1efa1b4bc8d41d072b3dbc
ParodiFranco <parodifranco00@gmial.com>
2025-04-23 15:09:38 -0300

    Added tests for fmemset and fmemcmp.

---
+++ fstring/tests/fmemcmp.X/test_fmemcmp.c
@@ -0,0 +1,50 @@
+/*============================================================================
+* ET: embedded test;
+============================================================================*/
+#include "fstring.h" /* Code Under Test (CUT) */
+#include "et.h" /* ET: embedded test */
+
+#include <string.h> /* memcmp */
+
+void setup(void) {
+    /* executed before *every* non-skipped test */
+}
+
+void teardown(void) {
+    /* executed after *every* non-skipped and non-failing test */
+}
+
+/* test group --------------------------------------------------------------*/
+TEST_GROUP("fstring") {
+
+TEST("Different data, size = 0 test (passing)") {
+	int dif = fmemcmp("hola", "wwww", 0);
+	
+	VERIFY(dif == 0);
+}
+
+TEST("Different data, size = 2 test (passing)") {
+	int dif = fmemcmp("hola", "hwww", 2);
+	
+	VERIFY(dif == 'o' - 'w');
+}
+
+TEST("Different data, size = 3 test (passing)") {
+	int dif = fmemcmp("hola", "hwww", 3);
+	
+	VERIFY(dif == 'o' - 'w');
+}
+
+TEST("Same data, size = 0 test (passing)") {
+	int dif = fmemcmp("hola", "hola", 0);
+	
+	VERIFY(dif == 0);
+}
+
+TEST("Same data, size = 2 test (passing)") {
+	int dif = fmemcmp("hola", "hola", 2);
+	
+	VERIFY(dif == 0);
+}
+
+}
---
+++ fstring/tests/fmemset.X/test_fmemset.c
@@ -0,0 +1,34 @@
+/*============================================================================
+* ET: embedded test;
+============================================================================*/
+#include "fstring.h" /* Code Under Test (CUT) */
+#include "et.h" /* ET: embedded test */
+
+#include <string.h> /* memcmp */
+
+void setup(void) {
+    /* executed before *every* non-skipped test */
+}
+
+void teardown(void) {
+    /* executed after *every* non-skipped and non-failing test */
+}
+
+/* test group --------------------------------------------------------------*/
+TEST_GROUP("fstring") {
+
+TEST("fmemset: cnt = 0 test (passing)") {
+    char buf[5] = "....";
+	fmemset(buf, 'a', 0);
+	
+	VERIFY(memcmp(buf, "....", sizeof(buf)) == 0);
+}
+
+TEST("fmemset: cnt = 2 test (passing)") {
+	char buf[5] = "....";
+	fmemset(buf, 'a', 2);
+	
+	VERIFY(memcmp(buf, "aa..", sizeof(buf)) == 0);
+}
+
+}
